037c9fa42db2b64a71bcc7ad05018fd2a28f25e9
Gerard Verbeek <gerard.verbeek@luminis.eu>
2016-09-07 19:41:19 +0200

    Add swagger ui

--- evoluting-world-service/src/main/java/eu/luminis/PlanetApplication.java
+++ evoluting-world-service/src/main/java/eu/luminis/PlanetApplication.java
@@ -4,11 +4,18 @@ import eu.luminis.service.NameService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAsync
 @EnableDiscoveryClient
+@EnableSwagger2
 public class PlanetApplication {
 
 	public static void main(String[] args) {
@@ -31,5 +38,15 @@ public class PlanetApplication {
 		if (args.length > 1 && !args[0].equals("--spring.output.ansi.enabled=always")) {
 			System.setProperty("server.port", args[1]);
 		}
+	}
+
+	@Bean
+	public Docket swaggerSettings() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/");
 	}
 }
