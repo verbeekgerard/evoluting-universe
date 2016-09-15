6f6d06e5d09d273e17eca8852f95c4b38cc724e2
Gerard Verbeek <gerard.verbeek@luminis.eu>
2016-09-15 19:54:22 +0200

    Check if elasticsearch is available

--- Planet-Manager/src/main/java/eu/luminis/service/ElasticsearchExportStatsService.java
+++ Planet-Manager/src/main/java/eu/luminis/service/ElasticsearchExportStatsService.java
@@ -3,6 +3,7 @@ package eu.luminis.service;
 import eu.luminis.config.ElasticsearchConfig;
 import eu.luminis.domain.KnownPlanet;
 import eu.luminis.domain.Stats;
+import org.elasticsearch.client.transport.NoNodeAvailableException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.PostConstruct;
+import java.net.UnknownHostException;
 
 @Service
 public class ElasticsearchExportStatsService implements ExportStatsService {
@@ -23,38 +24,38 @@ public class ElasticsearchExportStatsService implements ExportStatsService {
 
     @Autowired
     private ElasticsearchConfig elasticSearchConfig;
-
     private ElasticsearchTemplate template;
 
 
     @PostConstruct
-    public void initIndex()
-    {
-//        try {
-//            template = elasticSearchConfig.elasticsearchTemplate();
-//        } catch (UnknownHostException e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//
-//        // Create an index if necessary
-//        if (!template.indexExists(INDEX_NAME)){
-//            template.createIndex(INDEX_NAME);
-//        }
-//        // Tell ELK to consider StockQuotation as a entity to use
-//        template.putMapping(Stats.class);
-//        template.refresh(INDEX_NAME);
+    public void initIndex() {
+        try {
+            template = elasticSearchConfig.elasticsearchTemplate();
+            if (!template.indexExists(INDEX_NAME)) {
+                template.createIndex(INDEX_NAME);
+            }
+            template.putMapping(Stats.class);
+            template.refresh(INDEX_NAME);
+        } catch (NoNodeAvailableException e) {
+            log.error("No elasticsearch node available!", e);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void export(KnownPlanet planet) {
         Stats stats = planet.getStats();
-        log.debug("exporting stats: {}", stats);
         IndexQuery query = new IndexQuery();
         query.setIndexName(INDEX_NAME);
         query.setType(planet.getName());
         query.setObject(stats);
-        template.index(query);
+        try {
+            template.index(query);
+            log.debug("exporting stats: {}", stats);
+        }catch (NoNodeAvailableException e){
+            log.warn("No stats exported to ElasticSearch! Check if the node(s) are available.");
+        }
     }
+
 }
--- Planet-Manager/src/main/java/eu/luminis/task/ExportStatsTask.java
+++ Planet-Manager/src/main/java/eu/luminis/task/ExportStatsTask.java
@@ -1,23 +1,23 @@
 package eu.luminis.task;
 
+import eu.luminis.domain.KnownPlanet;
 import eu.luminis.service.ExportStatsService;
 import eu.luminis.service.PlanetRegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class ExportStatsTask {
 
-    @Autowired
-    PlanetRegisterService planetRegisterService;
-
-    @Autowired
-    ExportStatsService exportStatsService;
+    @Autowired PlanetRegisterService planetRegisterService;
+    @Autowired ExportStatsService exportStatsService;
 
     @Scheduled(fixedDelayString = "${application.task.exportStats.interval}")
     private void getStats(){
-//        List<KnownPlanet> startedPlanets = planetRegisterService.getStartedKnownPlanets();
-//        startedPlanets.stream().forEach(p-> exportStatsService.export(p));
+        List<KnownPlanet> startedPlanets = planetRegisterService.getStartedKnownPlanets();
+        startedPlanets.stream().forEach(p-> exportStatsService.export(p));
     }
 }
